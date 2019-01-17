cef9e1a3a517154b738980b6bf1de5bf19e5079f
mld3 <wiensj@umich.edu>
2019-01-16 21:12:44 -0500

    Add files via upload

---
+++ h_wrkshp_ha/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='h_wrkshp_ha',
+      version='0.1',
+      description='helper file for Hack Aotearoa Workshop',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
